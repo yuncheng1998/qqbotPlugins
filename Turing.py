e4272e20aab979665fce510a809eee85bec6a78b
yuncheng <yuncheng1998@icloud.com>
2018-04-27 15:10:07 +0800

    图灵机器人

--- Turing.py
+++ Turing.py
@@ -1,16 +1,17 @@
-#-*- coding��utf-8 -*-
 import urllib.request
 import re
 
 
 def Turning(content):
-    postdata = urllib.parse.urlencode(
-        {'key': '12e3d822fdc0400b8fb79d8599dc799e', 'info': content}).encode('utf-8')
-    req = urllib.request.Request(
-        "http://www.tuling123.com/openapi/api", postdata)
-    r = urllib.request.urlopen(req).read().decode('utf-8')
-    r = r.replace('图灵机器人', 'Fanir')
-    r1 = re.search(r'"text":"([\s\S]*)"', r)
+    url = "http://www.tuling123.com/openapi/api"
+    postdata = urllib.parse.urlencode({
+        'key': '12e3d822fdc0400b8fb79d8599dc799e',
+        'info': content
+    }).encode('utf-8')
+
+    response = urllib.request.urlopen(url, postdata)
+    html = response.read().decode('utf-8')
+    r1 = re.search(r'"text":"([\s\S]*)"', html)
     return r1.group(1)
 
 
@@ -18,12 +19,8 @@ def onQQMessage(bot, contact, member, content):
     if content == '再见':
         bot.SendTo(contact, 'close, byebye')
         bot.Stop()
-    elif re.search('膜', content) is not None:
-        bot.SendTo(contact, '太暴力了')
-    elif re.search('苟', content) is not None:
-        bot.SendTo(contact, '苟利国家生死以(滑稽)')
-    elif re.search('蛤', content) is not None:
-        bot.SendTo(contact, '蛤蛤')
+    elif re.search('索思', content) is not None:
+        bot.SendTo(contact, '索思科技协会')
     elif re.search('我是谁', content) is not None:
         bot.SendTo(contact, '你是' + str(contact))
     elif re.match('/help', content) is None:
